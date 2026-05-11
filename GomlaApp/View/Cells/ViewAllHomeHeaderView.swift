75b81cc69098eb8a97bdc4d016f8f814a58ef3ce
daniel-halan <daniel.azmy@halan.com>
2026-05-11 17:14:31 +0300

    creating header view

---
+++ GomlaApp/View/Cells/ViewAllHomeHeaderView.swift
@@ -0,0 +1,49 @@
+//
+//  ViewAllHomeHeaderView.swift
+//  GomlaApp
+//
+//  Created by Daniel azmy on 06/05/2026.
+//
+
+import UIKit
+
+class ViewAllHomeHeaderView: UICollectionReusableView {
+    
+    //MARK: - UI components
+    private var headerTitle: UILabel = {
+       let label = UILabel()
+        label.font = .systemFont(ofSize: 20, weight: .medium)
+        label.textColor = .black
+        label.numberOfLines = 1
+        label.textAlignment = .left
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupView()
+        setupConstraints()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    //MARK: - Functions
+    private func setupView(){
+        addSubview(headerTitle)
+    }
+    
+    private func setupConstraints(){
+        NSLayoutConstraint.activate([
+            headerTitle.topAnchor.constraint(equalTo: topAnchor),
+            headerTitle.leadingAnchor.constraint(equalTo: leadingAnchor)
+        ])
+    }
+    
+    //MARK: - Configuration
+    func configure(title: String){
+        headerTitle.text = title
+    }
+}
